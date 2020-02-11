e05d24c3154658501d044cd0bb932356cb141102
SamirGits <31531792+SamirGits@users.noreply.github.com>
2020-02-11 15:01:52 +0530

    Add files via upload

---
+++ Spring/Spring.web.mvc/WebContent/Spring-Webb-mvc/src/controller/Student.java
@@ -0,0 +1,22 @@
+package controller;
+
+public class Student {
+
+	public void setUname(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getEmail() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getUname() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
